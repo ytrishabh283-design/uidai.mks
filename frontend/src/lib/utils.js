ee7dedf55feae0417c9efaae3e8c3f643f7beaaf
ytrishabh283-design <ytrishabh283@gmail.com>
2026-04-18 13:19:55 +0530

    Update utils.js

--- frontend/src/lib/utils.js
+++ frontend/src/lib/utils.js
@@ -1,10 +1,5 @@
 import { clsx } from "clsx";
 import { twMerge } from "tailwind-merge"
-def verify_password(plain_password: str, hashed_password: str) -> bool:
-    return plain_password == hashed_password
-
-def get_password_hash(password: str) -> str:
-    return password
 export function cn(...inputs) {
   return twMerge(clsx(inputs));
 }
