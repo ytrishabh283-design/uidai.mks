81dcec3c5cfee5bc0d346dce664c50b934c24bc2
ytrishabh283-design <ytrishabh283@gmail.com>
2026-04-20 21:49:54 +0530

    Update and rename profile.js to Profile.js

---
+++ frontend/src/pages/Profile.js
@@ -0,0 +1,11 @@
+export default function Profile() {
+  return (
+    <div className="p-6">
+      <h1 className="text-2xl font-bold mb-4">Profile Page</h1>
+      <div className="bg-white rounded-xl shadow p-6">
+        <p><strong>Name:</strong> Rishabh Pathak</p>
+        <p><strong>Staff ID:</strong> JEPC_MKS_GAR_NS900297</p>
+      </div>
+    </div>
+  );
+}
