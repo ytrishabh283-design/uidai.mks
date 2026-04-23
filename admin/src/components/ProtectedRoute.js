75f703fc9e35cd1b426fa04db777075af0decfc5
ytrishabh283-design <ytrishabh283@gmail.com>
2026-04-23 19:41:58 +0530

    Add ProtectedRoute component for authentication

--- admin/src/components/ProtectedRoute.js
+++ admin/src/components/ProtectedRoute.js
@@ -1,1 +1,10 @@
+import React from "react";
+import { Navigate } from "react-router-dom";
 
+export default function ProtectedRoute({ isAuthenticated, children }) {
+  if (!isAuthenticated) {
+    return <Navigate to="/login" replace />;
+  }
+
+  return children;
+}
