f6b3cc499307918840d0e395d629e1473a535fc3
ytrishabh283-design <ytrishabh283@gmail.com>
2026-04-23 19:42:15 +0530

    Update Layout.js

--- admin/src/components/Layout.js
+++ admin/src/components/Layout.js
@@ -1,1 +1,22 @@
+import React, { useState } from "react";
+import Sidebar from "./Sidebar";
+import Topbar from "./Topbar";
 
+export default function Layout({ children, admin, onLogout }) {
+  const [sidebarOpen, setSidebarOpen] = useState(true);
+
+  return (
+    <div className="min-h-screen bg-gray-100 flex">
+      <Sidebar
+        sidebarOpen={sidebarOpen}
+        setSidebarOpen={setSidebarOpen}
+        onLogout={onLogout}
+      />
+
+      <div className="flex-1 flex flex-col min-w-0">
+        <Topbar admin={admin} />
+        <main className="p-4 md:p-6">{children}</main>
+      </div>
+    </div>
+  );
+}
