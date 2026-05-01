9fb94a4f5304467de014ffb3f6c4977fefe99c66
ytrishabh283-design <ytrishabh283@gmail.com>
2026-05-01 15:08:54 +0530

    Update App.js

--- admin/src/App.js
+++ admin/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Login from "./pages/Login";
 import Dashboard from "./pages/Dashboard";
 import Users from "./pages/Users";
+import Requests from "./pages/Requests";
 import Reports from "./pages/Reports";
 import Settings from "./pages/Settings";
 import Layout from "./components/Layout";
