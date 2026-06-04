758b4dedd19c749025afc1f080476dfa234b6bb4
ytrishabh283-design <ytrishabh283@gmail.com>
2026-06-05 01:26:25 +0530

    Add files via upload

--- admin/src/components/Layout.js
+++ admin/src/components/Layout.js
@@ -6,16 +6,16 @@ export default function Layout({ children, admin, onLogout }) {
   const [sidebarOpen, setSidebarOpen] = useState(true);
 
   return (
-    <div className="min-h-screen bg-gray-100 flex">
+    <div className="h-screen bg-gray-100 flex overflow-hidden">
       <Sidebar
         sidebarOpen={sidebarOpen}
         setSidebarOpen={setSidebarOpen}
         onLogout={onLogout}
       />
 
-      <div className="flex-1 flex flex-col min-w-0">
+      <div className="flex-1 flex flex-col min-w-0 overflow-hidden">
         <Topbar admin={admin} />
-        <main className="p-4 md:p-6">{children}</main>
+        <main className="flex-1 overflow-y-auto p-4 md:p-6">{children}</main>
       </div>
     </div>
   );
